ee6d8cdfb607ef261a07476953af28536f4e8764
Prateek Tandon <prateek1.tandon@gmail.com>
2022-06-08 20:04:24 -0700

    v0.0.0.3.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name="ocr-ops",
-    version="0.0.0.3.7",
+    version="0.0.0.3.8",
     author="Prateek Tandon",
     author_email="prateek1.tandon@gmail.com",
     description="OCR-Ops",
